aff15e4a24428faa99943898d3ad030f81d87c35
Yasuharu Nakano <ynak@jggug.org>
2015-11-05 13:34:55 +0900

    Keep only the way to modify bean properties in Config.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,15 +1,9 @@
 class BootStrap {
 
     def messageSource
-    def grailsApplication
 
     def init = { servletContext ->
-        //println messageSource.dump()
-        // デフォルトの18nファイル用のMessageSourceビーンの設定を変更する
-        //messageSource.basenames = grailsApplication.config.i18n.messages
-        //messageSource.defaultEncoding = "UTF-8"
-
-        println messageSource.dump() // for DEBUG
+        println "messageSource: " + messageSource.dump() // for DEBUG
     }
 
     def destroy = {
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,17 +1,2 @@
-import org.springframework.context.support.ReloadableResourceBundleMessageSource
-import grails.util.Environment
-
 beans = {
-    // 自前でi18nファイル用のMessageSourceビーンを上書き定義する場合
-    //messageSource(ReloadableResourceBundleMessageSource) {
-    //    fallbackToSystemLocale = false
-    //    defaultEncoding = "UTF-8"
-    //    basenames = grailsApplication.config.i18n.messages
-    //
-    //    // 開発環境のみ一定間隔ごとに読みなおす(自動リロード)
-    //    // WARデプロイ時など開発環境以外ではメッセージの反映に再起動が必要。
-    //    if (Environment.developmentMode) {
-    //        cacheSeconds = 5 // sec
-    //    }
-    //}
 }
