b2aa655a5722573e2870fb3fd5b27421838ade51
Yasuharu Nakano <ynak@jggug.org>
2015-11-05 10:47:17 +0900

    Define ReloadableResourceBundleMessageSource bean with external message files' path

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,18 @@
-// Place your Spring DSL code here
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import grails.util.Environment
+
 beans = {
+    // 追加のi18nファイルを指定するためのMessageSourceビーン。
+    messageSource(ReloadableResourceBundleMessageSource) {
+        fallbackToSystemLocale = false
+        fileEncodings = "UTF-8"
+        defaultEncoding = "UTF-8"
+        basenames = grailsApplication.config.additional.messages
+
+        // 開発環境のみ一定間隔ごとに読みなおす(自動リロード)
+        // WARデプロイ時など開発環境以外ではメッセージの反映に再起動が必要。
+        if (Environment.developmentMode) {
+            cacheSeconds = 5 // sec
+        }
+    }
 }
