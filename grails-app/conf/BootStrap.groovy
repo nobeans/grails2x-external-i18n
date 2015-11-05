2f1910708b9d0f26428f158817dd2f3acc93d9cd
Yasuharu Nakano <ynak@jggug.org>
2015-11-05 13:32:42 +0900

    Implement by three ways

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
 class BootStrap {
 
+    def messageSource
+    def grailsApplication
+
     def init = { servletContext ->
+        //println messageSource.dump()
+        // デフォルトの18nファイル用のMessageSourceビーンの設定を変更する
+        //messageSource.basenames = grailsApplication.config.i18n.messages
+        //messageSource.defaultEncoding = "UTF-8"
+
+        println messageSource.dump() // for DEBUG
     }
+
     def destroy = {
     }
 }
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,17 +2,16 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import grails.util.Environment
 
 beans = {
-    // 追加のi18nファイルを指定するためのMessageSourceビーン。
-    messageSource(ReloadableResourceBundleMessageSource) {
-        fallbackToSystemLocale = false
-        fileEncodings = "UTF-8"
-        defaultEncoding = "UTF-8"
-        basenames = grailsApplication.config.additional.messages
-
-        // 開発環境のみ一定間隔ごとに読みなおす(自動リロード)
-        // WARデプロイ時など開発環境以外ではメッセージの反映に再起動が必要。
-        if (Environment.developmentMode) {
-            cacheSeconds = 5 // sec
-        }
-    }
+    // 自前でi18nファイル用のMessageSourceビーンを上書き定義する場合
+    //messageSource(ReloadableResourceBundleMessageSource) {
+    //    fallbackToSystemLocale = false
+    //    defaultEncoding = "UTF-8"
+    //    basenames = grailsApplication.config.i18n.messages
+    //
+    //    // 開発環境のみ一定間隔ごとに読みなおす(自動リロード)
+    //    // WARデプロイ時など開発環境以外ではメッセージの反映に再起動が必要。
+    //    if (Environment.developmentMode) {
+    //        cacheSeconds = 5 // sec
+    //    }
+    //}
 }
